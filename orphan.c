2daf2b07942358460561460afdf9c99673288fe2
aniketkumar <kaniket123456789@gmail.com>
2023-12-04 19:11:47 +0530

    added return values of child

--- orphan.c
+++ orphan.c
@@ -6,18 +6,22 @@
 
 int main(){
 
+    int s;
     pid_t p;
     printf("program starts!\n");
     p=fork();
 
     if(p==0){
-        sleep(5);
         printf("child process:my pid is %d\n",getpid());
         printf("child process:my parent pid is %d\n",getppid());
+        return 50;
+       
     }
     else{
         printf("Parent process:my child's id is %d\n",p);
         printf("Parent process:My pid is %d\n",getpid());
+        wait(&s);
+        printf("return value:%d\n",WEXITSTATUS(s));
     }
 
     printf("common\n");
