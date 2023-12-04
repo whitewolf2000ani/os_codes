e90379b486e141a4e759a9f9b786b6f41c244240
aniketkumar <kaniket123456789@gmail.com>
2023-12-04 16:00:34 +0530

    added and understood dup and dup2

--- dup_concept.c
+++ dup_concept.c
@@ -13,11 +13,14 @@ int main()
 
     old_fd = open("new.txt", O_RDWR | O_CREAT, 0777);
     printf("Give a input for the file\n");
-    read(0, buff, 10);
-    write(old_fd, buff, 10);
+    int n=read(0, buff, 10);
+    printf("%d\n",n);
+    write(old_fd, buff, n);
     int new_fd = dup2(old_fd, 7);
     printf("old fd:%d\n", old_fd);
     printf("new fd:%d\n", new_fd);
+    int pin=lseek(new_fd,0,SEEK_SET);
+    printf("%d\n",pin);
     int buff2_read=read(new_fd,buff2,10);
     printf("%d\n",buff2_read);
     write(1, buff2, 10);
